f6092f51ff63e9768933106fc76e6058efc01e38
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2017-10-11 21:45:44 -0500

    Add HMR

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[main].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
